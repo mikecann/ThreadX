cc68e7cda941b1b035011a68f680f28789652ae1
Mike Cann <mike.cann@gmail.com>
2023-07-29 15:22:07 +0800

    removing this ignore too

--- blueprint-templates/component/__pascalCase_name__.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-interface Props {
-  children?: React.ReactNode;
-}
-
-export const {{pascalCase name}}: React.FC<Props> = ({children }) => {
-  return <Box>{children}</Box>;
-}
